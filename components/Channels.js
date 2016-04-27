35fc99ececcc8e56ee3ee3658e5fa6df318a58b8
datmar <darius.atmar@gmail.com>
2016-04-27 15:23:37 -0400

    Add all files

---
+++ components/Channels.js
@@ -0,0 +1,84 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var Modal = require('react-modal')
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+var Channels = React.createClass({
+
+  getInitialState: function() {
+    return { modalIsOpen: false };
+  },
+
+  openModal: function() {
+    this.setState({modalIsOpen: true});
+  },
+
+  afterOpenModal: function() {
+    // references are now sync'd and can be accessed.
+    this.refs.subtitle.style.color = '#f00';
+  },
+
+  closeModal: function() {
+    this.setState({modalIsOpen: false});
+  },
+
+  joinNewChannel: function() {
+  	var new_channel = $("#new-channel-name").val();
+  	if (new_channel.trim() != "") {
+	  	this.props.createChannel(new_channel);
+	  	this.closeModal();
+  	}
+  },
+
+  switchChannel: function(channelName) {
+  	this.props.joinChannel(channelName);
+  },
+
+	render: function() {
+		var that = this;
+		var currentChannel = this.props.currentChannel;
+		var channelList = this.props.channels.map(function(channel, i) {
+			return(
+				<li key={i}className={channel === currentChannel ? "channel active" : "channel"} onClick={that.switchChannel.bind(that, channel)}>
+					<a className="channel_name">
+						<span className="unread">0</span>
+						<span><span className="prefix">#</span>{channel}</span>
+					</a>
+				</li>
+			)
+		})
+
+		return(
+			<div className="listings_channels">
+				<span className="add_icon" onClick={this.openModal}>+</span>
+				<h2 className="listings_header">Channels</h2>
+				<ul className="channel_list">
+					{channelList}
+				</ul>
+	        <Modal
+	          isOpen={this.state.modalIsOpen}
+	          onRequestClose={this.closeModal}
+	          style={customStyles} >
+
+	          <h2 className="text-center">Enter a Channel Name</h2>
+	          <div>
+	          	<input id="new-channel-name" type="text" />
+		          <button className="btn" onClick={this.joinNewChannel}>Join</button>
+		         </div>
+	        </Modal>
+			</div>
+		)
+	}
+})
+module.exports = Channels;
